f01ca243b3dfbb95ab859b7a0c9c444f06c50809
ThomasTestaud <thomas.testaud@hotmail.fr>
2024-05-07 11:25:03 +0200

    pwa update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
     <Header />
+    <NuxtPwaManifest />
     <div class="bg-gray-100 pt-4 pb-12 min-h-[100dvh]">
         <slot />
     </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,47 @@
+import { installPrompt } from "virtual:nuxt-pwa-configuration";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", 'nuxt-icon']
+  modules: ["@nuxtjs/tailwindcss", 'nuxt-icon', '@vite-pwa/nuxt'],
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: "Burger Drive",
+      short_name: "BurgerDrive",
+      description: "Burge Drive app",
+      lang: "en",
+      theme_color: "#FF0000",
+      icons: [
+        {
+          src: 'static/icon.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'static/icon.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'static/icon.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      // periodicSyncForUpdates: 20,
+    },
+  }
 })
